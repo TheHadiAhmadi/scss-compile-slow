afb17bfb916cbb3081d9f97519cea7d2344ead8a
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-06-22 19:49:22 +0430

    update

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,4 +1,5 @@
 <slot/>
 <style lang="scss" global>
     @import '@tabler/core/src/scss/tabler';
+    @import '../scss'
 </style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,17 @@
 <script>
 
 import Button from "../lib/Button.svelte";
+import Input from "../lib/Input.svelte";
 
 </script>
 
 <!-- try to change color and wait to reload -->
 <Button color="green">Button</Button>
-<Button color="blue">Button</Button>
-<Button color="purple">Button</Button>
+<Button color="info">Button</Button>
+<Button color="danger">Button</Button>
+<Button color="purple m-3">Button</Button>
+<Button color="purple m-3">Button</Button>
+<Button color="purple m-5">Button</Button>
+<Button color="red m-1">Button</Button>
+
+<Input/>
